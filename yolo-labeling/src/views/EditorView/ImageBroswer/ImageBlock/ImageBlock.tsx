1fae3e5018554e874485fab34bb1574f7b1399d8
sc-bin <3335447573@qq.com>
2025-05-30 15:02:02 +0800

    优化缩略图浏览页面

--- yolo-labeling/src/views/EditorView/ImageBroswer/ImageBlock/ImageBlock.tsx
+++ yolo-labeling/src/views/EditorView/ImageBroswer/ImageBlock/ImageBlock.tsx
@@ -1,9 +1,5 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import './ImageBlock.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { getCurrentIndex, getImageList } from '../../../../store/ImageList/selectors';
-import { selectImage } from '../../../../store/ImageList/actions';
-import { getLabelStates } from "../../../../store/LabelState/selectors";
 
 export interface LabelClass {
     x: number;
@@ -18,60 +14,46 @@ interface ImageBlockProps {
     imageUrl: string;
     index: number;
     labels: LabelClass[];
+    isCurrent: boolean;
+    onclick: () => void;
 }
 
-const ImageBlock: React.FC<ImageBlockProps> = ({ altText, imageUrl, index, labels }) => {
-    const [isSelected, setIsSelected] = useState<boolean>(false);
+const ImageBlock: React.FC<ImageBlockProps> = ({ altText, imageUrl, index, labels, isCurrent, onclick }) => {
     const BlockRef = useRef<HTMLDivElement>(null);
-    const currentIndex = useSelector(getCurrentIndex);
-    const ImageList = useSelector(getImageList);
-    const LabelState = useSelector(getLabelStates);
-
-    const dispatch = useDispatch();
-
     function generateLabelBoxes() {
         let bs: JSX.Element[] = [];
-        let labels = ImageList[index].labels;
         for (let i = 0; i < labels.length; i++) {
-            let xywh = labels[i].xywh
-            if (xywh != null) {
-                bs[i] = <div className="TagBlock"
-                    key={i}
-                    style={{
-                        position: 'absolute',
-                        left: xywh[0] * 100 + "%",
-                        top: xywh[1] * 100 + "%",
-                        width: xywh[2] * 100 + "%",
-                        height: xywh[3] * 100 + "%",
-                        transform: 'translate(-50%, -50%)',
-                        border: '2px solid ' + LabelState[labels[i].labelIndex].color.toString(),
-                    }}
-                >
-                </div>
-            }
+            let label = labels[i]
+            bs[i] = <div className="TagBlock"
+                key={i}
+                style={{
+                    position: 'absolute',
+                    left: label.x * 100 + "%",
+                    top: label.y * 100 + "%",
+                    width: label.width * 100 + "%",
+                    height: label.height * 100 + "%",
+                    transform: 'translate(-50%, -50%)',
+                    border: '2px solid ' + label.color.toString(),
+                }}
+            >
+            </div>
         }
         return bs;
     }
 
-
-    const handleClick = () => {
-        setIsSelected(!isSelected);
-        dispatch(selectImage(index))
-
-    };
     return (
         <div
             className={`ImageBlock`}
-            onClick={handleClick}
+            onClick={onclick}
             ref={BlockRef}
             style={{
-                border: currentIndex === index ? '5px solid white' : 'none',
+                border: isCurrent ? '5px solid white' : 'none',
             }}
         >
             <h3
                 className='ImageBlockName'
                 style={{
-                    background: ImageList[index].labels.length > 0 ? "green" : '',
+                    background: labels.length > 0 ? "green" : '',
                 }}>{altText}</h3>
 
             <div className='ImageBlockImage' >
--- yolo-labeling/src/views/EditorView/ImageBroswer/ImageBroswer.tsx
+++ yolo-labeling/src/views/EditorView/ImageBroswer/ImageBroswer.tsx
@@ -2,18 +2,41 @@ import React from 'react';
 import './ImageBroswer.css';
 import ImageBlock, { type LabelClass } from './ImageBlock/ImageBlock'
 import ImageAdd from './ImageAdd/ImageAdd'
-import { useSelector } from "react-redux";
+import { useSelector,useDispatch } from "react-redux";
 import { getImageList } from "../../../store/ImageList/selectors";
-
+import { getLabelStates } from "../../../store/LabelState/selectors";
+import { getCurrentIndex } from '../../../store/ImageList/selectors';
+import { selectImage } from '../../../store/ImageList/actions';
 
 const ImageBroswer: React.FC = () => {
+    const dispatch = useDispatch();
     const ImageList = useSelector(getImageList);
+    const LabelStates = useSelector(getLabelStates);
+    const currentIndex = useSelector(getCurrentIndex);
 
     let bs: JSX.Element[] = [];
     for (let i = 0; i < ImageList.length; i++) {
-        let labels: LabelClass[];
-        
-        bs[i] = <ImageBlock key={i} altText={ImageList[i].fileName} imageUrl={ImageList[i].imageUrl} index={i} labels={labels} />
+        let labels: LabelClass[] = [];
+        for (let j = 0; j < ImageList[i].labels.length; j++) {
+            const labelIndex = ImageList[i].labels[j].labelIndex
+            const xywh = ImageList[i].labels[j].xywh
+            if (xywh) {
+                labels.push({
+                    x: xywh[0],
+                    y: xywh[1],
+                    width: xywh[2],
+                    height: xywh[3],
+                    color: LabelStates[labelIndex].color
+                })
+            }
+
+        }
+
+        bs[i] = <ImageBlock key={i}
+            altText={ImageList[i].fileName} imageUrl={ImageList[i].imageUrl}
+            index={i} labels={labels}
+            isCurrent={i === currentIndex} 
+            onclick={() => dispatch(selectImage(i))} />
     }
     return (
         <div className="ImageBroswer">
